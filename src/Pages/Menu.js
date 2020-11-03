0ec70244c590f85d31a9f7b97137faf732de7d9b
Kayli Garwood <kgarwood@uncc.edu>
2020-11-02 21:58:34 -0500

    Added pages to app

---
+++ src/Pages/Menu.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import {
+    Link
+  } from "react-router-dom";
+
+function Menu() {
+  return (
+    <div>
+        <nav>
+      <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/dashboard">Dashboard</Link></li>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/signup">Sign up</Link></li>
+      </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Menu;
