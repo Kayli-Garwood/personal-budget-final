f4f67f5d9342bf2cf34b6f3f3a51ff22bfe3fd6f
Kayli Garwood <kgarwood@uncc.edu>
2020-12-17 14:26:00 -0500

    still editing part 2

--- src/Pages/DashboardPage.js
+++ src/Pages/DashboardPage.js
@@ -20,8 +20,8 @@ class DashboardPage extends Component {
 
   async componentDidMount() {
     let token = localStorage.getItem("jwt");
-    console.log("http://104.131.54.49/budget/"+localStorage.getItem("userId"))
-    const res = await axios.get("http://104.131.54.49/budget/"+localStorage.getItem("userId"), {
+    console.log("http://173.94.80.104/budget/"+localStorage.getItem("userId"))
+    const res = await axios.get("http://173.94.80.104/budget/"+localStorage.getItem("userId"), {
       headers: {
         'Authorization': `Bearer ${token}`,
       },
